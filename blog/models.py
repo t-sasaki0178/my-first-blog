fc34c006eb17994991eeaaa7c416b96cc75f8771
佐々木剛志 <t_sasaki@sasakitsuyoshinoMacBook-puro.local>
2018-11-05 15:13:35 +0900

    database and html template

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User','on_delete')
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length=200) #テキスト数を定義するフィールド
     text = models.TextField() #制限なしの長いテキスト用。ブログの投稿用？
     created_date = models.DateTimeField(default=timezone.now) #日付と時間のフィールド
-    publish_date = models.DateTimeField(blank=True,null=True) #他モデルへのリンク
+    published_date = models.DateTimeField(blank=True,null=True) #他モデルへのリンク
 
     def publish(self):
         self.published_date = timezone.now()
